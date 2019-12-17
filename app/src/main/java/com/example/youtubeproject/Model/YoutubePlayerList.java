6227d929e12649dfb61e99096c5a4bbf0929f2e2
kimjinuk <sdf7895@naver.com>
2019-12-18 08:09:57 +0900

    Parcelable

--- app/src/main/java/com/example/youtubeproject/Model/YoutubePlayerList.java
+++ app/src/main/java/com/example/youtubeproject/Model/YoutubePlayerList.java
@@ -1,6 +1,9 @@
 package com.example.youtubeproject.Model;
 
-public class YoutubePlayerList {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class YoutubePlayerList implements Parcelable {
 
     public String title;
     public String url;
@@ -16,6 +19,41 @@ public class YoutubePlayerList {
         this.state = state;
     }
 
+
+    protected YoutubePlayerList(Parcel in) {
+        title = in.readString();
+        url = in.readString();
+        id = in.readString();
+        duration = in.readString();
+        state = in.readByte() != 0;
+    }
+
+    public static final Creator<YoutubePlayerList> CREATOR = new Creator<YoutubePlayerList>() {
+        @Override
+        public YoutubePlayerList createFromParcel(Parcel in) {
+            return new YoutubePlayerList(in);
+        }
+
+        @Override
+        public YoutubePlayerList[] newArray(int size) {
+            return new YoutubePlayerList[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(url);
+        dest.writeString(id);
+        dest.writeString(duration);
+        dest.writeByte((byte) (state ? 1 : 0));
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
     //----------------------SET----------------------
     public void setTitle(String title) {this.title = title;}
     public void setDuration(String duration) {this.duration = duration;}
@@ -29,4 +67,5 @@ public class YoutubePlayerList {
     public String getUrl() {return url;}
     public String getId() {return id;}
     public boolean isState() {return state;}
-}
+
+} //보낼땐 putExtra 받을땐 getParcelableExtra
